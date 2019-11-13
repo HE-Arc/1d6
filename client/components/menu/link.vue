af56fcfabe75c894eb4f818d640418458e42f89f
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-11-13 16:14:48 +0100

    Add basic navbar

---
+++ client/components/menu/link.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    :href="link"
+    class="navbar-item"
+  >
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    'link': {type: String, required: true}
+  }
+};
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,8 +1,79 @@
 <template>
   <div>
-    <nuxt />
+    <nav
+      class="navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a
+          class="navbar-item"
+          href=""
+        >
+          <img
+            src="https://cdn.discordapp.com/icons/628956181922185246/5c74d1ba261f479dfa5ec5a357107ebd.webp?size=128"
+          > 
+          <menu-link>Indécis</menu-link>
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+      >
+        <div class="navbar-end">
+          <menu-link
+            link="vote"
+          >
+            Create a poll
+          </menu-link>
+          
+          <menu-link
+            link="groups"
+          >
+            Groups
+          </menu-link>
+
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <main>
+      <nuxt />
+    </main>
   </div>
 </template>
+
+<script>
+import menuLink from '~/components/menu/link';
+
+export default {
+  components: {
+    menuLink
+  }
+};
+</script>
 
 <style>
 html {
@@ -15,6 +86,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  background-color: #eee;
 }
 
 *,
@@ -51,5 +123,15 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+
+main {
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 20px;
+  padding: 20px;
+  border-radius: 1px;
+  height: 100%;
+  background-color: white;
 }
 </style>
