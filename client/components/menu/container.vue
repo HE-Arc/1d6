8b61d532d2d5d6c48c729a814a48a5a8b586f2ef
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-11-13 16:15:05 +0100

    Add temp container demo

---
+++ client/components/menu/container.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  <!-- TODO "Card" container for some items -->
+  </div>
+</template>
+
+<script>
+export default {
+  props: {}
+};
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,72 +1,38 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        1d6
-      </h1>
-      <h2 class="subtitle">
-        Bloated app helping you to make choices
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <div class="container">
+      <span class="header">Active polls</span>
+      No active polls
+    </div>
+
+
+    <div class="container">
+      <span class="header">Past polls</span>
+      No past polls
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+// import Logo from '~/components/Logo.vue';
 
 export default {
   components: {
-    Logo
+    
   }
-}
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container {
+    border-left: 4px solid #ccc;
+    padding-left: 20px;
+    margin-bottom: 20px;
+    margin-top: 20px;
+  }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+  .header {
+    display: block;
+    font-size: 1.3em;
+  }
 </style>
