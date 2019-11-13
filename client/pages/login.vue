a59859035892c5917fddd9a982a0310f9b1dbaab
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-11-13 22:35:31 +0100

    Add basic login pages

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,233 +1,32 @@
 <template>
   <div>
     <div class="field">
-      <label class="label">Name</label>
-      <div class="control">
-        <input
-          class="input"
-          type="text"
-          placeholder="Text input"
-        >
-      </div>
-    </div>
-
-    <div class="field">
       <label class="label">Username</label>
-      <div class="control has-icons-left has-icons-right">
+      <div class="control">
         <input
           class="input is-success"
           type="text"
-          placeholder="Text input"
-          value="bulma"
-        >
-        <span class="icon is-small is-left">
-          <i class="fas fa-user" />
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-check" />
-        </span>
-      </div>
-      <p class="help is-success">
-        This username is available
-      </p>
-    </div>
-
-    <div class="field">
-      <label class="label">Email</label>
-      <div class="control has-icons-left has-icons-right">
-        <input
-          class="input is-danger"
-          type="email"
-          placeholder="Email input"
-          value="hello@"
-        >
-        <span class="icon is-small is-left">
-          <i class="fas fa-envelope" />
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-exclamation-triangle" />
-        </span>
-      </div>
-      <p class="help is-danger">
-        This email is invalid
-      </p>
-    </div>
-
-    <div class="field">
-      <label class="label">Subject</label>
-      <div class="control">
-        <div class="select">
-          <select>
-            <option>Select dropdown</option>
-            <option>With options</option>
-          </select>
-        </div>
-      </div>
-    </div>
-
-    <div class="field">
-      <label class="label">Message</label>
-      <div class="control">
-        <textarea
-          class="textarea"
-          placeholder="Textarea"
-        />
-      </div>
-    </div>
-
-    <div class="field">
-      <div class="control">
-        <label class="checkbox">
-          <input type="checkbox">
-          I agree to the <a href="#">terms and conditions</a>
-        </label>
-      </div>
-    </div>
-
-    <div class="field">
-      <div class="control">
-        <label class="radio">
-          <input
-            type="radio"
-            name="question"
-          >
-          Yes
-        </label>
-        <label class="radio">
-          <input
-            type="radio"
-            name="question"
-          >
-          No
-        </label>
-      </div>
-    </div>
-
-    <div class="field is-grouped">
-      <div class="control">
-        <button class="button is-link">
-          Submit
-        </button>
-      </div>
-      <div class="control">
-        <button class="button is-link is-light">
-          Cancel
-        </button>
-      </div>
-    </div><div class="field">
-      <label class="label">Name</label>
-      <div class="control">
-        <input
-          class="input"
-          type="text"
-          placeholder="Text input"
+          placeholder="Username"
         >
       </div>
     </div>
-
     <div class="field">
-      <label class="label">Username</label>
-      <div class="control has-icons-left has-icons-right">
+      <label class="label">Password</label>
+      <div class="control">
         <input
           class="input is-success"
-          type="text"
-          placeholder="Text input"
-          value="bulma"
+          type="password"
+          placeholder="Password"
         >
-        <span class="icon is-small is-left">
-          <i class="fas fa-user" />
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-check" />
-        </span>
-      </div>
-      <p class="help is-success">
-        This username is available
-      </p>
-    </div>
-
-    <div class="field">
-      <label class="label">Email</label>
-      <div class="control has-icons-left has-icons-right">
-        <input
-          class="input is-danger"
-          type="email"
-          placeholder="Email input"
-          value="hello@"
-        >
-        <span class="icon is-small is-left">
-          <i class="fas fa-envelope" />
-        </span>
-        <span class="icon is-small is-right">
-          <i class="fas fa-exclamation-triangle" />
-        </span>
-      </div>
-      <p class="help is-danger">
-        This email is invalid
-      </p>
-    </div>
-
-    <div class="field">
-      <label class="label">Subject</label>
-      <div class="control">
-        <div class="select">
-          <select>
-            <option>Select dropdown</option>
-            <option>With options</option>
-          </select>
-        </div>
       </div>
     </div>
 
-    <div class="field">
-      <label class="label">Message</label>
-      <div class="control">
-        <textarea
-          class="textarea"
-          placeholder="Textarea"
-        />
-      </div>
-    </div>
-
-    <div class="field">
-      <div class="control">
-        <label class="checkbox">
-          <input type="checkbox">
-          I agree to the <a href="#">terms and conditions</a>
-        </label>
-      </div>
-    </div>
-
-    <div class="field">
-      <div class="control">
-        <label class="radio">
-          <input
-            type="radio"
-            name="question"
-          >
-          Yes
-        </label>
-        <label class="radio">
-          <input
-            type="radio"
-            name="question"
-          >
-          No
-        </label>
-      </div>
-    </div>
-
-    <div class="field is-grouped">
-      <div class="control">
+    <div class="field is-grouped is-grouped-right">
+      <p class="control">
         <button class="button is-link">
-          Submit
+          Login
         </button>
-      </div>
-      <div class="control">
-        <button class="button is-link is-light">
-          Cancel
-        </button>
-      </div>
+      </p>
     </div>
   </div>
 </template>
@@ -242,6 +41,6 @@ this.$auth.loginWith('local', {
 })
 */
 export default {
-  
+  layout: 'login'
 };
 </script>
