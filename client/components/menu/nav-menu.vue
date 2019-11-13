4957213c5533130d0912d70eddbed481a2876a70
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-11-13 17:55:01 +0100

    Separate navbar from default vue

---
+++ client/components/menu/nav-menu.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav
+    class="navbar"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a
+        class="navbar-item"
+        href=""
+      >
+        <img
+          src="https://cdn.discordapp.com/icons/628956181922185246/5c74d1ba261f479dfa5ec5a357107ebd.webp?size=128"
+        > 
+        <menu-link>Indécis</menu-link>
+      </a>
+
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+
+    <div
+      id="navbarBasicExample"
+      class="navbar-menu"
+    >
+      <div class="navbar-end">
+        <menu-link
+          link="vote"
+        >
+          Create a poll
+        </menu-link>
+          
+        <menu-link
+          link="groups"
+        >
+          Groups
+        </menu-link>
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <a
+              class="button is-primary"
+              href="register"
+            >
+              <strong>Sign up</strong>
+            </a>
+            <a
+              class="button is-light"
+              href="login"
+            >
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import menuLink from '~/components/menu/link';
+
+export default {
+  components: {
+    menuLink
+  }
+};
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,64 +1,6 @@
 <template>
   <div>
-    <nav
-      class="navbar"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href=""
-        >
-          <img
-            src="https://cdn.discordapp.com/icons/628956181922185246/5c74d1ba261f479dfa5ec5a357107ebd.webp?size=128"
-          > 
-          <menu-link>Indécis</menu-link>
-        </a>
-
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-        >
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
-
-      <div
-        id="navbarBasicExample"
-        class="navbar-menu"
-      >
-        <div class="navbar-end">
-          <menu-link
-            link="vote"
-          >
-            Create a poll
-          </menu-link>
-          
-          <menu-link
-            link="groups"
-          >
-            Groups
-          </menu-link>
-
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a class="button is-light">
-                Log in
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
+    <nav-menu />
     <main>
       <nuxt />
     </main>
@@ -66,11 +8,11 @@
 </template>
 
 <script>
-import menuLink from '~/components/menu/link';
+import navMenu from '~/components/menu/nav-menu';
 
 export default {
   components: {
-    menuLink
+    navMenu
   }
 };
 </script>
