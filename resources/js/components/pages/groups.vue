177acbd7f34b7dd73c7c3bd7ad0f56417830a675
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-12-03 10:35:48 +0100

    Close #39

--- resources/js/components/pages/groups.vue
+++ resources/js/components/pages/groups.vue
@@ -1,17 +1,24 @@
 <template>
   <default-layout>
-      <p>Groups?</p>
+    <h1>Your groups</h1>
+    <div class="columns is-multiline is-1-mobile is-0-tablet is-3-desktop is-8-widescreen container-container">
+      <div class="column container-border poll-card is-one-quarter">
+        <span class="header">Mangeur de lel</span>
+        <b>5</b> peoples<br>
+        <span class="poll-card-action"><a href="#"><i class="fa fa-edit"></i> Edit</a></span>
+        <span class="poll-card-action"><a href="#"><i class="fa fa-trash"></i> Delete</a></span>
+        <span class="poll-card-action"><a href="#"><i class="fa fa-star"></i> Stars</a></span>
+      </div>
+    </div>
+    <button class="button is-primary is-rounded floating-button">New group</button>
   </default-layout>
 </template>
 
 <script>
 export default {
-  components: {
-    
-  }
+  components: {}
 };
 </script>
 
 <style>
-
 </style>
