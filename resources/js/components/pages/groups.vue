5fd14602feb41b17d88315d3183e79b48e25841f
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-12-07 17:20:26 +0100

    Close #19

--- resources/js/components/pages/groups.vue
+++ resources/js/components/pages/groups.vue
@@ -1,5 +1,23 @@
 <template>
   <default-layout>
+    <div class="modal is-active">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Edit default ratings</p>
+          <button class="delete" aria-label="close"></button>
+        </header>
+        <section class="modal-card-body">
+          <div class="item-list">
+            <select-ratings />
+          </div>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success">Save</button>
+          <button class="button">Cancel</button>
+        </footer>
+      </div>
+    </div>
     <div class="modal">
       <div class="modal-background"></div>
       <div class="modal-card">
@@ -10,7 +28,7 @@
         <section class="modal-card-body">
           <form>
             <label class="label">Group name</label>
-            <input type="text" placeholder="Where are we gonna eat" class="input input-group-name"/>
+            <input type="text" placeholder="Where are we gonna eat" class="input input-group-name" />
             <div class="columns">
               <div class="column is-half">
                 <select-list icon="fa-user-plus" placeholder="Member name">Members</select-list>
@@ -28,20 +46,48 @@
       </div>
     </div>
     <h1>Your groups</h1>
-    <div class="columns is-multiline is-1-mobile is-0-tablet is-3-desktop is-8-widescreen container-container">
+    <div
+      class="columns is-multiline is-1-mobile is-0-tablet is-3-desktop is-8-widescreen container-container"
+    >
       <div class="column container-border poll-card is-one-quarter">
         <span class="header">Mangeur de lel</span>
-        <b>5</b> members<br>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-edit"></i> Edit</a></span>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-trash"></i> Delete</a></span>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-star"></i> Stars</a></span>
+        <b>5</b> members
+        <br />
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-edit"></i> Edit
+          </a>
+        </span>
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-trash"></i> Delete
+          </a>
+        </span>
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-star"></i> Stars
+          </a>
+        </span>
       </div>
       <div class="column container-border poll-card is-one-quarter">
         <span class="header">Mangeur de lel</span>
-        <b>5</b> members<br>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-edit"></i> Edit</a></span>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-trash"></i> Delete</a></span>
-        <span class="poll-card-action"><a href="#"><i class="fa fa-star"></i> Stars</a></span>
+        <b>5</b> members
+        <br />
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-edit"></i> Edit
+          </a>
+        </span>
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-trash"></i> Delete
+          </a>
+        </span>
+        <span class="poll-card-action">
+          <a href="#">
+            <i class="fa fa-star"></i> Stars
+          </a>
+        </span>
       </div>
     </div>
     <button class="button is-primary is-rounded floating-button">New group</button>
@@ -49,12 +95,11 @@
 </template>
 
 <script>
-
-import selectList from '../groups/select-list';
+import selectRatings from "../items/select-ratings";
 
 export default {
   components: {
-    selectList
+    selectRatings
   }
 };
 </script>
@@ -73,5 +118,10 @@ export default {
 .delete-item {
   margin-right: 12px;
   float: right;
+}
+
+.item-list {
+  max-height: 400px;
+  overflow: auto;
 }
 </style>
