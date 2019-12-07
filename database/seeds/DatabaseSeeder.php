e3def7e41e730bb6b8aaa28c437d082a406ccf61
Pierre Bürki <pierre.burki@he-arc.ch>
2019-12-07 09:39:12 +0100

    Add seeders call in DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            GroupsTableSeeder::class,
+            ItemsTableSeeder::class,
+            GroupUsersTableSeeder::class,
+            GroupItemsTableSeeder::class,
+            ItemDefaultRatingsTableSeeder::class,
+            ]);
     }
 }
