8c83b8554b1c8c8405a7a0dd1aa138f7e4361df4
Pierre Bürki <pierre.burki@he-arc.ch>
2019-12-07 18:37:46 +0100

    Add undeletable elements in select-list

--- resources/js/components/groups/list-item.vue
+++ resources/js/components/groups/list-item.vue
@@ -1,8 +1,8 @@
 <template>
 <span class="list-item">
     <slot></slot>
-    <a class="delete-item">
-    <i class="fa fa-trash"></i>
+    <a v-bind:class="{'is-active' : cannotDelete}" href="#" class="delete-item" v-on:click="deleteEntry()">
+    <i v-if="!cannotDelete" class="fa fa-trash"></i>
     </a>
 </span>
 </template>
@@ -10,7 +10,14 @@
 <script>
 export default {
   components: {},
-  props: ["name"]
+  props: ["name", "cannotDelete"],
+  methods: {
+    deleteEntry() {
+      let items = this.$parent.items;
+      let index = items.indexOf(this.$slots.default[0].text);
+      items.splice(index, 1);
+    },
+  }
 };
 </script>
 
