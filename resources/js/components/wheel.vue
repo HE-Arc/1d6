a5e068cd13ee06f08d25af3958cc574d8f906bc4
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-12-10 09:59:17 +0100

    Fix wheel rotation

--- resources/js/components/wheel.vue
+++ resources/js/components/wheel.vue
@@ -12,17 +12,17 @@ const colors = [
   "#48c774"
 ];
 
-let playingAnimation = true;
-let tick = 0;
-let acceleration = 0;
-let speed = 0;
-let currentAngle = 0;
-let findCorrectSlowDown = false;
-let ticksToStopTheWheel = -1;
-let targetWeight = 0;
+const ROTATION_COUNT = 30;
+const DECELERATION_COEFF = 0.01;
+
+let duration = 0;
+let currentAngle = Math.PI * 0.5;
+let targetAngle = Math.PI * 0.5;
+let ctx, canvas;
+let items = [];
 
 // Get a center point and a radius for a given canvas. Circle will fit in the center of the canvas
-function getCircle(canvas) {
+function getCircle() {
   return {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -30,24 +30,26 @@ function getCircle(canvas) {
   };
 }
 
-function spin(targetWeight_) {
-  previsionalDistTraveled = -1;
-  targetWeight = targetWeight_;
-  acceleration = 0.005;
-  findCorrectSlowDown = false;
-
-  setTimeout(() => {
-    acceleration = 0;
-    findCorrectSlowDown = true;
-  }, 1000);
+// Start spinning the wheel
+function spin(targetWeight) {
+  targetAngle = toRad(0.75 - targetWeight + ROTATION_COUNT);
+  currentAngle = Math.PI * 0.5;
+  render();
 }
 
-window.spin = spin;
+// Calculate where the wheel should stop
+function handleWheelRotation() {
+  let t = (currentAngle + 1) / targetAngle;
+  currentAngle += DECELERATION_COEFF * t * (3 - 2 * t) * (targetAngle - currentAngle); //(t * t * (3 - 2 * t)) * targetAngle;
+
+  return targetAngle !== currentAngle;
+}
+
 // Draw a part of a pie
-function drawPiePart(canvas, ctx, c, start, size, color, radius, lineWidth) {
+function drawPiePart(c, start, size, color, radius, lineWidth) {
   // Where we start / end the pie angle
-  start = start * 2 * Math.PI;
-  let end = start + size * 2 * Math.PI;
+  start = toRad(start);
+  let end = start + toRad(size);
 
   // We use a trick to render nice "parts" by using a large line width
   ctx.lineWidth = lineWidth;
@@ -57,70 +59,17 @@ function drawPiePart(canvas, ctx, c, start, size, color, radius, lineWidth) {
   ctx.stroke();
 }
 
-let previsionalDistTraveled = -1;
-function handleCurrentAngle() {
-  if (ticksToStopTheWheel >= 0) {
-    ticksToStopTheWheel--;
-  } else if (ticksToStopTheWheel > -1) {
-    ticksToStopTheWheel--;
-    speed = 0;
-    acceleration = 0;
-  }
-
-  speed += acceleration;
-  currentAngle += speed;
-
-  if (findCorrectSlowDown) {
-    const slowDownAcceleration = -0.001;
-    // Time in ticks to reach 0
-    let tempTicks = speed / -slowDownAcceleration;
-
-    let distTraveled = 0;
-    let tempCurrentSpeed = speed;
-
-    // Compute this only once because the speed does not change
-    if (previsionalDistTraveled === -1) {
-      // "Quickly" compute how much we will travel with a speed of -0.001
-      for (let i = 0; i < tempTicks; i++) {
-        tempCurrentSpeed += slowDownAcceleration;
-        distTraveled += tempCurrentSpeed;
-      }
-
-      previsionalDistTraveled = distTraveled;
-    }
-
-    const stopAngle = (/*0.5 * Math.PI + */currentAngle + previsionalDistTraveled) % (2 * Math.PI);
-
-    const targetWeightInRadian = 2 * Math.PI * targetWeight;
-    const angleDiff = Math.abs(targetWeightInRadian - stopAngle);
-
-    // Assume 1 deg is enough precision
-    if (angleDiff <= 0.017) {
-      findCorrectSlowDown = false;
-      acceleration = slowDownAcceleration;
-      ticksToStopTheWheel = tempTicks;
-    }
-  }
-}
-
 // Main render "loop"
-function render(ctx, canvas, items) {
-  const c = getCircle(canvas);
+function render() {
+  const c = getCircle();
 
-  handleCurrentAngle();
-
-  renderBackground(ctx, canvas, c);
-  renderItems(ctx, canvas, c, items);
-
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, 100, 150);
-  ctx.fillStyle = "black";
-  ctx.fillText(currentAngle % (2 * Math.PI), 100, 100);
+  renderBackground(c);
+  renderItems(c);
 
   // Saves battery on mobile by only rendering when we need it
-  if (playingAnimation) {
+  if (handleWheelRotation()) {
     requestAnimationFrame(() => {
-      render(ctx, canvas, items);
+      render();
     });
   }
 }
@@ -130,7 +79,7 @@ function toRad(x) {
 }
 
 // Render all items present in the pie. Weights must sum up to 1!
-function renderItems(ctx, canvas, c, items) {
+function renderItems(c) {
   let sum = 0;
 
   ctx.textBaseline = "middle";
@@ -148,8 +97,6 @@ function renderItems(ctx, canvas, c, items) {
 
     // Pie parts
     drawPiePart(
-      canvas,
-      ctx,
       c,
       sum,
       item.weight,
@@ -194,7 +141,6 @@ function renderItems(ctx, canvas, c, items) {
 }
 
 function drawPiePartText(ctx, c, sum, item) {
-  //ctx.translate(0,0);
   ctx.rotate(toRad(sum - item.weight / 2));
 
   ctx.fillStyle = "black";
@@ -209,10 +155,9 @@ function drawPiePartText(ctx, c, sum, item) {
   ctx.fillText(item.name, c.r * 0.75, 0, c.r * 0.5 - 5);
 
   ctx.rotate(-toRad(sum - item.weight / 2));
-  //ctx.translate(-c.x, -c.y);
 }
 
-function renderBackground(ctx, canvas, c) {
+function renderBackground(c) {
   // Background
   ctx.fillStyle = "#eee";
   ctx.beginPath();
@@ -229,8 +174,8 @@ function renderBackground(ctx, canvas, c) {
 
 export default {
   mounted() {
-    const canvas = document.getElementById("wheel-canvas");
-    const ctx = canvas.getContext("2d");
+    canvas = document.getElementById("wheel-canvas");
+    ctx = canvas.getContext("2d");
 
     // Make sure canvas is a square
     canvas.width = canvas.offsetWidth;
