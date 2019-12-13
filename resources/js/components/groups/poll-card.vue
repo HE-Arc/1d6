82b6fb4b0ca2b5b3872ea78f93b5edcccd5807e0
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-12-13 16:24:42 +0100

    Add poll-card

---
+++ resources/js/components/groups/poll-card.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="column container-border container-active poll-card is-one-quarter">
+    <span class="header">{{name}}</span>
+    <b>{{readyUserCount}} / {{totalUserCount}}</b> users voted
+    <br />
+    <a href="#">
+      <span v-show="!isClosed">Take part to the poll</span>
+      <span v-show="isClosed">View results</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "id", "totalUserCount", "readyUserCount", "isClosed"]
+};
+</script>
