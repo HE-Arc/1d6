8fcc3575cab4ca210f30c8079e072175e4e4b757
Loïck Jeanneret <lock.jeanneret@gmail.com>
2019-12-16 00:20:21 +0100

    Fix being able to send invalid ratings values

--- app/Http/Controllers/PollController.php
+++ app/Http/Controllers/PollController.php
@@ -9,6 +9,7 @@ use App\Poll;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use \Validator;
 
 class PollController extends Controller
 {
@@ -41,22 +42,22 @@ class PollController extends Controller
 
         if (count($items) > 0) {
             try {
-            $users = addLoggedUserToData(jsonDecodeToArray($request->users, true), "id", Auth::id(), function (&$arr, $index) {
-                // Make sure owner is admin
-                $arr[$index]["admin"] = true;
-            }, function (&$arr) {
-                $arr[] = ["id" => Auth::id(), "admin" => true];
-            });
+                $users = addLoggedUserToData(jsonDecodeToArray($request->users, true), "id", Auth::id(), function (&$arr, $index) {
+                    // Make sure owner is admin
+                    $arr[$index]["admin"] = true;
+                }, function (&$arr) {
+                    $arr[] = ["id" => Auth::id(), "admin" => true];
+                });
 
-            // TODO: Check if it is not possible to do only one query
-            foreach ($users as $key => $user) {
-                $poll->users()->sync([$user["id"] => ['admin' => $user["admin"]]], false);
-            }
+                // TODO: Check if it is not possible to do only one query
+                foreach ($users as $key => $user) {
+                    $poll->users()->sync([$user["id"] => ['admin' => $user["admin"]]], false);
+                }
 
-            foreach ($items as $key => $item) {
-                // TODO: Security issue here, see #113
+                foreach ($items as $key => $item) {
+                    // TODO: Security issue here, see #113
                     $poll->items()->attach($item->id);
-            }
+                }
             } catch (\Throwable $th) {
                 // If anything fails, delete the poll
                 $poll->delete();
@@ -64,7 +65,7 @@ class PollController extends Controller
         } else {
             $poll->delete();
             return response()->json(["errors" => ["Cannot create poll without items."]], 401);
-         }
+        }
 
         return response()->json(json_encode(["id" => $poll->id]));
     }
@@ -78,6 +79,13 @@ class PollController extends Controller
      */
     public function rate(Request $request, int $pollId)
     {
+        $ratings = jsonDecodeToArray($request->ratings, true);
+        $validator = Validator::make($ratings, ['*.rating' => 'integer|required|max:10|min:0']);
+
+        if ($validator->passes()) {
+            // Can only vote if we are in the poll
+            $poll = Poll::find($pollId);
+
             if ($poll->users()->find(Auth::id())->exists()) {
                 // TODO: Check how to do only 1 request
                 foreach ($ratings as $rating) {
@@ -86,15 +94,20 @@ class PollController extends Controller
                         // TODO: Fix doing so many queries here
                         // TODO: Allow user to update their vote
                         // TODO: Do not use insert directly, find a way to make *many to many to many* properly or at least make this query nicer
-            DB::insert('insert into poll_ratings (poll_id, user_id, item_id, rating) values (?, ?, ?, ?)', [$pollId, Auth::id(), $rating['id'], $rating['rating']]);
-        }
+                        DB::insert('insert into poll_ratings (poll_id, user_id, item_id, rating) values (?, ?, ?, ?)', [$pollId, Auth::id(), $rating['id'], $rating['rating']]);
+                    }
                     else {
                         return response()->json(["errors" => ["Item does not exists in poll."]], 401);
                     }
-    }
+                }
             } else {
                 return response()->json(["errors" => ["Cannot vote in this poll."]], 401);
             }
+            return null;
+        } else {
+            return response()->json(["errors" => ["Invalid rating range."]], 401);
+        }
+    }
 
     /**
      * Display the specified resource.
