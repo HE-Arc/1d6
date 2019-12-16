c2ea6afe4ea17bccce4fbcb2bec54d6047c5fe5c
John Leuba <john.leuba@he-arc.ch>
2019-12-16 17:53:17 +0100

    Close #115

--- app/Http/Resources/PollResourceCollection.php
+++ app/Http/Resources/PollResourceCollection.php
@@ -3,10 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
-class PollResourceLite extends JsonResource
+class PollResourceCollection extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -17,9 +16,12 @@ class PollResourceLite extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'total_user_count' => $this->users->count(),
             'user_count' => DB::table('poll_ratings')->select(DB::raw('count(DISTINCT user_id) as count'))->where('poll_id', $this->id)->first()->count,
             'chosen_item_id' => $this->chosen_item_id,
-            'items' => ItemResourceLite::collection($this->whenLoaded('items')),
+            'items' => ItemResource::collection($this->whenLoaded('items')),
         ];
     }
 }
